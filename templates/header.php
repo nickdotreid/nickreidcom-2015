0f2e0aa630ea28c5c26c6fbdad203170ec2b93cb
Nick Reid <nickreid@nickreid.com>
2015-03-11 18:12:58 -0400

    styled navigation bar

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <div class="container">
   <header class="banner" role="banner">
     <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="navbar" role="navigation">
+    <nav class="navbar navbar-default" role="navigation">
       <?php
         if (has_nav_menu('primary_navigation')) :
           wp_nav_menu(array('theme_location' => 'primary_navigation', 'walker' => new Roots_Nav_Walker(), 'menu_class' => 'nav navbar-nav'));
