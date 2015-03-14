1bc140b652541c3892b91416f11c0918a7a63d77
Nick Reid <nickreid@nickreid.com>
2015-03-14 11:33:44 -0400

    added false bottom to page

--- base.php
+++ base.php
@@ -1,23 +1,25 @@
 <?php get_template_part('templates/head'); ?>
 <body <?php body_class(); ?>>
 
-  <?php
-    do_action('get_header');
-    get_template_part('templates/header');
-  ?>
+  <div class="wrapper-top">
+    <?php
+      do_action('get_header');
+      get_template_part('templates/header');
+    ?>
 
-  <div class="wrap" role="document">
-    <div class="row">
-      <main class="main" role="main">
-        <?php include roots_template_path(); ?>
-      </main><!-- /.main -->
-      <?php if (roots_display_sidebar()) : ?>
-        <aside class="sidebar" role="complementary">
-          <?php include roots_sidebar_path(); ?>
-        </aside><!-- /.sidebar -->
-      <?php endif; ?>
-    </div><!-- /.content -->
-  </div><!-- /.wrap -->
+    <div class="wrap" role="document">
+      <div class="row">
+        <main class="main" role="main">
+          <?php include roots_template_path(); ?>
+        </main><!-- /.main -->
+        <?php if (roots_display_sidebar()) : ?>
+          <aside class="sidebar" role="complementary">
+            <?php include roots_sidebar_path(); ?>
+          </aside><!-- /.sidebar -->
+        <?php endif; ?>
+      </div><!-- /.content -->
+    </div><!-- /.wrap -->
+  </div><!-- /.container-wrap -->
 
   <?php get_template_part('templates/footer'); ?>
 
