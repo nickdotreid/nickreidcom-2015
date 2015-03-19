b41119fa0dbc4d2a6ff0a4141ed3181b559e8564
Nick Reid <nickreid@nickreid.com>
2015-03-18 22:47:43 -0400

    styled project gallery fields

--- project-gallery.php
+++ project-gallery.php
@@ -3,6 +3,33 @@ Template Name: Project Gallery
 */?>
 
 <?php while (have_posts()) : the_post(); ?>
+<div class="project-gallery-top">
   <?php get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/content', 'page'); ?>
+</div>
+<?php
+$projects = get_field('projects');
+if($projects):
+	foreach( $projects as $object):
+		$post = $object['project'];
+		setup_postdata($post);
+		?>
+<div class="project project-gallery-item">
+	<div class="header">
+		<h2><?php echo roots_title(); ?></h2>
+	</div>
+	<div class="content">
+		<?php the_content(); ?>
+	</div>
+	<div class="nav navbar-default">
+		<ul class="nav navbar-nav">
+			<li><a href="#">Something</a></li>
+		</ul>
+	</div>
+</div>
+		<?php
+	endforeach;
+	wp_reset_postdata();
+endif;
+?>
 <?php endwhile; ?>
