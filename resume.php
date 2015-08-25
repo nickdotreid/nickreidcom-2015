5fbc79e2538019d56f66291162122c877de7a840
Nick Reid <nickreid@nickreid.com>
2015-08-24 18:25:18 -0700

    added more content to resume

--- resume.php
+++ resume.php
@@ -4,9 +4,65 @@ Template Name: Resume Page
 <div class="alert alert-info">
 	Want to print my resume? <a href="#">Download a printable resume here.</a>
 </div>
-<div class="resume-section-large">
-	<h2>Health Communication Designer and Developer</h2>
+<div class="section resume-section-large">
+	<h1 class="heading">Health Communication Designer and Developer</h1>
 	<div class="contnet">
 		<p>Creates health communication technologies that aid the continuous improvement of community health by simplifying problems and language that create functional and sustainable solutions.</p>
 	</div>
+</div>
+<div class="section resume-section">
+	<h2 class="heading">Education</h2>
+	<div class="row">
+		<div class="col-xs-6 content">
+			<div class="date">May 2015</div>
+			<div class="title">Master of Health Informatics</div>
+			<p>
+				University of Michigan<br />
+				Ann Arbor, MI
+			</p>
+		</div>
+		<div class="col-xs-6 content">
+			<div class="date">May 2008</div>
+			<div class="title">Bachelor of Art in Art Practice</div>
+			<p>
+				University of California at Berkeley<br />
+				Berkeley, CA
+			</p>
+		</div>
+	</div>
+</div>
+<div class="resume-section">
+	<h2 class="heading">Skills</h2>
+	<div class="content">
+		<p><strong>Love:</strong>Backbone.js, Illustrator, Gantt Charts, Plain Language, Power Point, Python, and UX</p>
+		<p><strong>Can do:</strong>After Effects, Axure, Node, Phonegap, PHP, R, Ruby, SASS, and Web Analytics</p>
+	</div>
+</div>
+<div class="resume-section">
+	<h2>Professional Experience</h2>
+	<div class="row content">
+		<div class="col-sm-4">
+			January 2015 – July 2015
+			Global Information Exchange Program
+			Development Lead, Fellow
+		</div>
+		<div class="col-sm-8">
+			With a health organization, iKure, we are developing technology to support maternal-child health efforts in rural India.
+		</div>
+	</div>
+	<div class="row content">
+		<div class="col-sm-4">
+			May 2014 – May 2015
+			University of Michigan Health System
+			CS Mott Children’s Hospital
+			Design Researcher, Web Developer
+		</div>
+		<div class="col-sm-8">
+			Supported adolescent type 1 diabetics by creating products with a participatory process.
+		</div>
+	</div>
+
+
+
+
 </div>
