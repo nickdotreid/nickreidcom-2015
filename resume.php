9f6316148f064986ea769e32ec6e8efc1ae9d56d
Nick Reid <nickreid@nickreid.com>
2015-08-24 17:47:05 -0700

    added simple start to resume page template

---
+++ resume.php
@@ -0,0 +1,12 @@
+<?/*
+Template Name: Resume Page
+*/?>
+<div class="alert alert-info">
+	Want to print my resume? <a href="#">Download a printable resume here.</a>
+</div>
+<div class="resume-section-large">
+	<h2>Health Communication Designer and Developer</h2>
+	<div class="contnet">
+		<p>Creates health communication technologies that aid the continuous improvement of community health by simplifying problems and language that create functional and sustainable solutions.</p>
+	</div>
+</div>
