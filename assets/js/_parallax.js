774e4ad16353f384e17cd43b3faa09e10893c8c2
Nick Reid <nickreid@nickreid.com>
2015-09-03 20:33:28 -0700

    first draft of parallax code

--- assets/js/_parallax.js
+++ assets/js/_parallax.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
-	
+	$('.parallax-midground, .parallax-background').css('position', 'relative').css('top', '0px');
+	$(window).scroll(function () {
+		var windowHeight = $(window).height();
+		var scrollTop = $(window).scrollTop();
+		var middlePage = scrollTop + windowHeight/2;
+		$('.parallax-container:visible').each(function(){
+			var container = $(this);
+			var containerMiddle = container.offset().top + container.height()/2;
+			var difference = middlePage - containerMiddle;
+			//var top = Number(div.css('top').replace('px', ''));
+			$('.parallax-midground', container).css('top', difference/20);
+			$('.parallax-background', container).css('top', difference/10);
+		});
+	});
 });
