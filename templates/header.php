befa9217a0aeafde91486274157e03b89c03038b
Nick Reid <nickreid@nickreid.com>
2015-09-11 13:11:15 -0400

    added classes to make nav sticky

--- templates/header.php
+++ templates/header.php
@@ -4,12 +4,15 @@
     <?php if (is_front_page()) {
       dynamic_sidebar('sidebar-banner');
     } ?>
-    <nav class="navbar navbar-default" role="navigation">
+    <nav class="navbar navbar-default sticky" role="navigation">
+      <a class="stuck-only icon-top">Back to top</a>
+      <div class="stuck-md">
       <?php
         if (has_nav_menu('primary_navigation')) :
           wp_nav_menu(array('theme_location' => 'primary_navigation', 'walker' => new Roots_Nav_Walker(), 'menu_class' => 'nav navbar-nav'));
         endif;
       ?>
+      </div>
     </nav>
   </header>
 </div>
